48697224afc146f91d4dbdbfc07d68ed81ea3fb8
CandiSucifta <muhammad.candisucifta@gmail.com>
2024-03-22 20:20:04 +0700

    membuat routing dengan 4 metode berbeda yaitu Get, Post, Put, Delete

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Home')
 })
 
 app.get('/about', (req, res) => {
     res.send('About')
+})
+
+app.get('/users', (req, res) => {
+    res.send('Get Users')
+})
+
+app.post('/users', (req, res) => {
+    res.send('Post Users')
+})
+
+app.put('/users', (req, res) => {
+    res.send('Put Users')
+})
+
+app.delete('/users', (req, res) => {
+    res.send('Delete Users')
 })
 
 app.listen(3000, () => {
